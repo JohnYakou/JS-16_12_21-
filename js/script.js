9075dd43c0c5fdc442cd09a2954d34b5b5de2f5a
YakouJohn <jyakou@gmx.fr>
2021-12-16 15:23:04 +0100

    16/12/21

---
+++ js/script.js
@@ -0,0 +1,2 @@
+let help = "help";
+console.log(help);
---
+++ js/variable.js
@@ -0,0 +1,8 @@
+"use strict";
+// commentaire 1
+
+// kamelCase : on commeence en minuscule et à chaque nouveaux mots on met une majuscule
+
+// une varible c'est un espace dans lequel on stock des choses, c'est une boite que l'on nomme
+var unTexte = "Voici une texte"; // ce qui est à froite donne sa valeur de ce qui est à gauche (par rapport au "=")
+console.log(unTexte);
